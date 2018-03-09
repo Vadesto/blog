9de02d4a4e7a7ded51d1f82ef90fcef6c93aee08
Vadesto <Vadeesto@gmail.com>
2018-03-09 16:27:33 +0700

    validates for Post added

---
+++ app/models/ability.rb
@@ -0,0 +1,16 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    def initialize(user)
+      can :read, Post   #guest
+      if user.present?  #logged in
+        can :manage, Post, user_id: user.id
+        if user.admin?  #admin
+          can :manage, :all
+        end
+      end
+    end
+  end
+
+end
