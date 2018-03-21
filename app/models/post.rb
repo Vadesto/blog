98a4d0eb8c6e8904d4dc2b94e42ff3b7cb3556a5
Vadesto <Vadeesto@gmail.com>
2018-03-21 23:13:08 +0700

    kaminari pagination

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
   validates :title, length: { in: 5..160 }, presence: true
   validates :text, presence: true
   validates :description, presence: true
+  paginates_per 5
 end
